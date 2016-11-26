2d01cf7e07619e4a7019b34252d40ecfd439c9fe
codehimanshu <himanshuagrawal1998@gmail.com>
2016-11-26 16:33:12 +0530

    fetch input and respond

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-	file_put_contents("fb.txt", file_get_contents("php://input"));
-	$fb = file_get_contents("fb.txt");
-	echo $fb;
+$accessToken = "EAASb5TsHYK8BABHqcIhdCEQKrCvQGm7d8EV3HaurBmQC6MPNq2ZB0DlvMXWVdnS45N9ZAwbUiJwtqWeLILjHIPSjTdVr7JdNTQc3iLCFFIla9t4iBpOO8F79kmx53IGLzslGgx38uftM0wqNPCsJcG7O3tNVJXv2rtUHUkJwZDZD";
+$input = json_decode(file_get_contents('php://input'), true);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$answer = "I don't understand. Ask me 'hi'.";
+if($messageText == "hi") {
+    $answer = "Hello";
+}
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_exec($ch);
+curl_close($ch);
 ?>
