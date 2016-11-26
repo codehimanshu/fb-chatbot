4140be83229d74b7c295fce5ab2f7202095c1261
codehimanshu <himanshuagrawal1998@gmail.com>
2016-11-27 00:01:21 +0530

    fix repetiotin bug

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
 $accessToken = getenv('token');
-// echo $_SERVER['token']."<br>";
-// $accessToken = "EAASb5TsHYK8BABHqcIhdCEQKrCvQGm7d8EV3HaurBmQC6MPNq2ZB0DlvMXWVdnS45N9ZAwbUiJwtqWeLILjHIPSjTdVr7JdNTQc3iLCFFIla9t4iBpOO8F79kmx53IGLzslGgx38uftM0wqNPCsJcG7O3tNVJXv2rtUHUkJwZDZD";
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $answer = "I don't understand. Ask me 'hi'.";
+if(!empty($messageText))
+{
 if($messageText == "hi") {
     $answer = "Hello";
 }
@@ -20,4 +19,5 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_exec($ch);
 curl_close($ch);
+}
 ?>
